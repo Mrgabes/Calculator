43733ab027ad13a6d006bd10faa3a43694b7a145
Gabriel Rodriguez <mrgabes72@gmail.com>
2021-06-01 11:12:31 -0700

    ADD HTML,JS,CSS, and math functions to JS.
    co-
    Authored-by: Danielle Trent <danielletrent@yahoo.com>

---
+++ JS/script.js
@@ -0,0 +1,43 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function sub(number1, number2) {
+  return number1 - number2;
+}
+
+function div(number1, number2) {
+  return number1 / number2;
+}
+
+function mul(number1, number2) {
+  return number1 * number2;
+}
+
+// const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
+// const result = add(number1, number2);
+//  alert(add(50,10));
+
+//  const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
+// const result = sub(number1, number2);
+//  alert(sub(number1,number2));
+
+ /*const number1 = parseInt(prompt("Enter a number:"));
+ const number2 = parseInt(prompt("Enter another number:"));
+ const result = div(number1, number2);
+  alert(div(number1,number2));*/
+
+  const number1 = parseInt(prompt("Enter a number:"));
+ const number2 = parseInt(prompt("Enter another number:"));
+ const result = mul(number1, number2);
+  alert(mul(number1,number2));
+
+ 
+
+
+
+
+
+

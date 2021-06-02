32fa59041e1dcb9aa6d62b74435b1f9dec7ff3e8
Gabriel Rodriguez <mrgabes72@gmail.com>
2021-06-02 15:12:35 -0700

    add mul,div,sub andhtml buttons

--- JS/script.js
+++ JS/script.js
@@ -14,15 +14,40 @@ function divide(number1, number2) {
   return number1 / number2;
 }
 
-$(document).ready(function() {
-  $("form#add").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
-    $("#output").text(result);
-  });
+$(document).ready(function () {
+  $(".add").click(function (event) {
+  event.preventDefault();
+  const number1 = parseInt($("#num1").val());
+  const number2 = parseInt($("#num2").val());
+  const result = add(number1, number2);
+  $("#output").text(result);
 });
+$(".subtract").click(function (event) {
+  event.preventDefault();
+  const number1 = parseInt($("#num1").val());
+  const number2 = parseInt($("#num2").val());
+  const result = subtract(number1, number2);
+  $("#output").text(result);
+});
+$(".multiply").click(function (event) {
+  event.preventDefault();
+  const number1 = parseInt($("#num1").val());
+  const number2 = parseInt($("#num2").val());
+  const result = multiply(number1, number2);
+  $("#output").text(result);
+});
+$(".divide").click(function (event) {
+  event.preventDefault();
+  const number1 = parseInt($("#num1").val());
+  const number2 = parseInt($("#num2").val());
+  const result = divide(number1, number2);
+  $("#output").text(result);
+});
+});
+
+
+
+
 // beverage below
 const beverage = $("#beverage").val();
 
